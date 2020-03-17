2d754afddabe6bf6d90c9ae5cb38586822556151
Jason <jason_skillman@outlook.com>
2020-03-17 09:37:30 -0400

    Created BaseAttribute class

---
+++ Assets/Scripts/BaseAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class BaseAttribute {
+
+    public int BaseValue {
+        get; private set;
+    }
+
+    public int BaseMultiplier {
+        get; private set;
+    }
+
+
+    public BaseAttribute(int baseValue, int baseMultiplier = 0) {
+        BaseValue = baseValue;
+        BaseMultiplier = baseMultiplier;
+    }
+
+}
