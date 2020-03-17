413a270829df3b422aff73752df9cd5c2208537c
Jason <jason_skillman@outlook.com>
2020-03-17 10:36:58 -0400

    Created driver class and example scene

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    [Header("Attributes")]
+    public int strengthValue = 10;
+
+    [Header("Level up Bonues")]
+    public int strengthBonus = 0;
+
+    private Attribute strengthAttribute;
+
+
+    private void Awake() {
+        strengthAttribute = new Attribute(strengthValue);
+    }
+
+    private void Start() {
+        //Add level up bonuses
+        strengthAttribute.AddRawBonus(new RawBonus(strengthBonus));
+    }
+
+    private void Update() {
+        Debug.Log("Strength Value: " + strengthAttribute.CalculateValue());
+    }
+
+}
