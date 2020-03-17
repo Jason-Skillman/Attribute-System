cc6388745cebc361cbd905c928317294b1ee41d4
Jason <jason_skillman@outlook.com>
2020-03-17 09:42:45 -0400

    Created RawBonus and FinalBonus classes

---
+++ Assets/Scripts/FinalBonus.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class FinalBonus : BaseAttribute {
+
+    public FinalBonus(int value = 0, int multiplier = 0) : base(value, multiplier) {
+
+    }
+
+}
---
+++ Assets/Scripts/RawBonus.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class RawBonus : BaseAttribute {
+    
+    public RawBonus(int value = 0, int multiplier = 0) : base(value, multiplier) {
+        
+    }
+
+}
